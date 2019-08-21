108ac22434bfa791d6e86a6b10c8519feadf49c8
Alumno <Alumno@PC05>
2019-08-21 12:08:51 -0500

    cambie el hola

--- hola/hola/Program.cs
+++ hola/hola/Program.cs
@@ -6,7 +6,7 @@ namespace hola
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Te amo amairani!");
         }
     }
 }
